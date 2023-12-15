dc267a9bb265a6f2c19c9426246ccf5f754383f0
Landi <joaomarcospsnbr@gmail.com>
2023-12-15 03:05:39 +0000

    API Twilio

--- main.py
+++ main.py
@@ -1,6 +1,26 @@
 from assistants import Marta
 from whatsapp import Whatsapp
+from twilio.twiml.messaging_response import Message, MessagingResponse
+from flask import Flask, request, jsonify
+
+app = Flask('app')
+
+@app.route('/')
+def index():
+    return 'API Online'
 
 
 
-print(Marta.response('flamento x santos, proximo jogo'))
+
+@app.route('/messages', methods=['POST'])
+def messages():
+    # request_data = request.get_data()
+    Whatsapp.response('CHad85b100f3024b1cad2fb2e841f9529d', Marta.response(request.form.get('Body')))
+    return response, 200
+
+if __name__ == '__main__':
+    app.run()
+
+
+# last_message = 'Qual a melhor taxa que InfinitePay oferece?'
+# Whatsapp.response('CHad85b100f3024b1cad2fb2e841f9529d', Marta.response(last_message))
--- whatsapp.py
+++ whatsapp.py
@@ -1,10 +1,8 @@
 from twilio.rest import Client
-from dotenv import load_dotenv
 import os
 
-load_dotenv()
-account_sid = os.getenv('TWILIO_ACCOUNT_SID')
-auth_token = os.getenv('TWILIO_AUTH_TOKEN')
+account_sid = os.environ.get('TWILIO_ACCOUNT_SID')
+auth_token = os.environ.get('TWILIO_AUTH_TOKEN')
 
 client = Client(account_sid, auth_token)
 
@@ -17,4 +15,4 @@ class Whatsapp():
                         .messages \
                         .create(author='system', body = message)
 
-Whatsapp.response('CHad85b100f3024b1cad2fb2e841f9529d', 'Olá, tudo bem?')
+# chat sid = 'CHad85b100f3024b1cad2fb2e841f9529d'
