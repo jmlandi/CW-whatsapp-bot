07a8ba372cba3f0f66809bd5cac9059118052a02
Landi <joao.sousa@cloudwalk.io>
2024-01-06 19:31:14 -0300

    executions configuration

--- models.py
+++ models.py
@@ -1,6 +1,6 @@
 from database import db
 
-class Executions(db.model):
+class Executions(db.Model):
     message_sid = db.Column(db.String(50), primary_key = True)
     execution_sid = db.Column(db.String(50))
     name = db.Column(db.String(30))
