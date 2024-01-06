19d2d1351a865f205a55955d96b94feda9a888a1
Landi <joao.sousa@cloudwalk.io>
2024-01-06 15:49:05 -0300

    twilio keys update

--- whatsapp.py
+++ whatsapp.py
@@ -1,12 +1,8 @@
 from twilio.rest import Client
 import os
 
-# account_sid = os.environ.get('TWILIO_ACCOUNT_SID')
-# auth_token = os.environ.get('TWILIO_AUTH_TOKEN')
-
-account_sid = 'AC15fac1d0add7090595bdda2dd3af4b03'
-auth_token = '7e60abe505ab92ed7bd7f980485ccaa4'
-
+account_sid = os.environ.get('TWILIO_ACCOUNT_SID')
+auth_token = os.environ.get('TWILIO_AUTH_TOKEN')
 
 client = Client(account_sid, auth_token)
 
