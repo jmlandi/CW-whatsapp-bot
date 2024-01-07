89128cb9add1b09e9b6328d71e7ca1847b375ff7
Landi <joao.sousa@cloudwalk.io>
2024-01-07 01:38:18 -0300

    chatbot flow changed to Twilio Webhook

--- controllers.py
+++ controllers.py
@@ -20,7 +20,6 @@ class Controller():
         contact = request.form.get('From')
         message_sid = request.form.get('MessageSid')
         body = request.form.get('Body')
-        content = request.get_data()
 
         if Prd.check_flow_execution(contact) == False:
             parameters = {
@@ -31,8 +30,7 @@ class Controller():
                 }
             Prd.create_flow(parameters, contact)
             return 200
-        
-        requests.post('https://webhooks.twilio.com/v1/Accounts/AC15fac1d0add7090595bdda2dd3af4b03/Flows/FW82751984a3114708b0ccfcf7b7d5a9ce', json=content)
+
         
     def ai_response():
         content = request.json
