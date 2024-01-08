37e5235a0210268a6ed8e63f891a155eab4f6874
Landi <joao.sousa@cloudwalk.io>
2024-01-07 23:20:27 -0300

    OpenAI Threads

--- controllers.py
+++ controllers.py
@@ -3,7 +3,6 @@ from models import Executions, Threads
 from assistants import Bruno
 from whatsapp import Prd
 from flask import render_template, request, jsonify
-import time
 
 class Controller():
     
@@ -22,14 +21,12 @@ class Controller():
             thread = Bruno.thread_init()
             Bruno.thread_message(thread, message)
             run = Bruno.thread_run(thread, "O produto InfinitePay em questão é: {topic}")
-            retrieve, cont_retrieve = Bruno.thread_retrieve_run(thread, run), 0
-            while retrieve.status != "completed":
+            retrieve = Bruno.thread_retrieve_run(thread, run)
+            while True:
                 retrieve = Bruno.thread_retrieve_run(thread, run)
-                time.sleep(0.5)
-                cont_retrieve += 1
-                if cont_retrieve > 5:
+                if retrieve == "completed":
+                    assistant_response = Bruno.thread_assitant_reponse(thread)
                     break
-            assistant_response = Bruno.thread_assitant_reponse(thread)
 
             new_thread = Threads(
                 flow_id = flow_id,
@@ -39,6 +36,7 @@ class Controller():
             db.session.add(new_thread)
             db.session.commit()
         
+        '''
         else:
             conversation = Threads.filter_by(flow_id=flow_id).first()
             Bruno.thread_message(conversation.thread_id, message)
@@ -51,8 +49,7 @@ class Controller():
                 if cont_retrieve > 5:
                     break
             assistant_response = Bruno.thread_assitant_reponse(conversation.thread_id)
+        '''
             
-
-
         response = {'response': assistant_response}
         return jsonify(response), 200
